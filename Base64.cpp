432e45e23fef9d8e997e12103ac84307eba36c89
Yang Dingpeng <yangdingpeng@vtron.com>
2018-08-03 12:01:59 +0800

    解码时改用查表法，提高效率

--- Base64.cpp
+++ Base64.cpp
@@ -16,6 +16,39 @@ const static char symbolTable[] = {
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
 };
+const static uint8_t valueTable[] = {
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*'+'*/0x3e,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*'/'*/0x3f,
+    /*'0'*/0x34, /*'1'*/0x35, /*'2'*/0x36, /*'3'*/0x37,
+    /*'4'*/0x38, /*'5'*/0x39, /*'6'*/0x3a, /*'7'*/0x3b,
+    /*'8'*/0x3c, /*'9'*/0x3d, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*'A'*/0x00, /*'B'*/0x01, /*'C'*/0x02,
+    /*'D'*/0x03, /*'E'*/0x04, /*'F'*/0x05, /*'G'*/0x06,
+    /*'H'*/0x07, /*'I'*/0x08, /*'J'*/0x09, /*'K'*/0x0a,
+    /*'L'*/0x0b, /*'M'*/0x0c, /*'N'*/0x0d, /*'O'*/0x0e,
+    /*'P'*/0x0f, /*'Q'*/0x10, /*'R'*/0x11, /*'S'*/0x12,
+    /*'T'*/0x13, /*'U'*/0x14, /*'V'*/0x15, /*'W'*/0x16,
+    /*'X'*/0x17, /*'Y'*/0x18, /*'Z'*/0x19, /*' '*/0xff,
+    /*' '*/0xff, /*' '*/0xff, /*' '*/0xff, /*' '*/0xff,
+    /*' '*/0xff, /*'a'*/0x1a, /*'b'*/0x1b, /*'c'*/0x1c,
+    /*'d'*/0x1d, /*'e'*/0x1e, /*'f'*/0x1f, /*'g'*/0x20,
+    /*'h'*/0x21, /*'i'*/0x22, /*'j'*/0x23, /*'k'*/0x24,
+    /*'l'*/0x25, /*'m'*/0x26, /*'n'*/0x27, /*'o'*/0x28,
+    /*'p'*/0x29, /*'q'*/0x2a, /*'r'*/0x2b, /*'s'*/0x2c,
+    /*'t'*/0x2d, /*'u'*/0x2e, /*'v'*/0x2f, /*'w'*/0x30,
+    /*'x'*/0x31, /*'y'*/0x32, /*'z'*/0x33,
+};
 
 Decoder::Decoder() {
     m_remainingSymbolCount = 0;
@@ -24,11 +57,10 @@ Decoder::Decoder() {
 
 uint8_t Decoder::symbol2value(char symbol) {
     uint8_t value = 0xff;
-    for (uint8_t i = 0; i < sizeof(symbolTable); i++) {
-        if (symbolTable[i] == symbol) {
-            value = i;
-            break;
-        }
+    uint8_t symbolIndex = (uint8_t)symbol;
+    //LOG_ERROR("%c => %d", symbol, symbolIndex);
+    if (symbolIndex < sizeof(valueTable)) {
+        value = valueTable[symbolIndex];
     }
     return value;
 }
